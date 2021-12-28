1207a2feae112541cfce69c687f3fbe08a86d470
Bhavesh <bhavesh511@outlok.com>
2021-12-28 17:01:42 +0530

    added model and Controller

---
+++ ExamDemo/Controllers/JWTAuthenticationServiceController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ExamDemo.Model;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ExamDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JWTAuthenticationServiceController : ControllerBase
+    {
+        public JWTAuthenticationServiceController()
+        {
+
+
+
+        }
+
+
+        private string GenerateToken(Users users)
+        {
+            //var SecretKey = Encoding.ASCII.GetBytes
+            //    ("Do not share this key");
+
+            var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, users.Email),
+                };
+
+            var JWTToken = new JwtSecurityToken(
+                issuer: "http://localhost:60064/",
+                audience: "http://localhost:60064/",
+                claims: authClaims,
+                notBefore: new DateTimeOffset(DateTime.Now).DateTime,
+                expires: new DateTimeOffset(DateTime.Now.AddDays(1)).DateTime,
+                //Using HS256 Algorithm to encrypt Token  
+                signingCredentials: new SigningCredentials
+                (new SymmetricSecurityKey(Encoding.ASCII.GetBytes("Do not share this key")), SecurityAlgorithms.HmacSha256Signature)
+                );
+            var token = new JwtSecurityTokenHandler().WriteToken(JWTToken);
+            return token;
+        }
+
+    }
+}
