b2c1b7f7b66b453bd5f65d89067fd9b15574ec33
Bhavesh <bhavesh511@outlok.com>
2021-12-28 15:58:38 +0530

    added token validation

---
+++ ExamDemo/Controllers/ExamRegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamRegistrationController : ControllerBase
+    {
+
+    }
+}
