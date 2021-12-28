df7ced0b6ff82efb073287f48dca50586f970f26
Hinal Bhuva <hinalbhuva031@gmail.com>
2021-12-28 17:57:17 +0530

    sp configuration

---
+++ ExamDemo/DataContext/IDataContextAsync.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExamDemo.DataContext
+{
+    public interface IDataContextAsync : IDataContext
+    {
+        //Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        Task<int> SaveChangesAsync();
+    }
+}
