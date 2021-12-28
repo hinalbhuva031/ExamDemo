55b5d2e13a521c46b0167fa4de1ae02573aaf409
Hinal Bhuva <hinalbhuva031@gmail.com>
2021-12-28 16:57:07 +0530

    added Contracts And Service folder

---
+++ ExamDemo/Contracts/IExamRegistrationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.Swagger;
+
+namespace ExamDemo.Contracts
+
+{
+    public interface IExamRegistrationService
+    {
+        Response CreateExamRegistration();
+    }
+}
